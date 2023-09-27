d2109db8b91b2cfd5371fa75d795db80324d4d82
Maratib Ali Khan <maratib@users.noreply.github.com>
2023-09-28 02:31:48 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <PdfViewer/>
+  </div>
+</template>
+
+<script>
+import PdfViewer from "./components/PdfViewer";
+
+export default {
+  name: "App",
+  components: {
+    PdfViewer
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: black;
+  padding: 50px;
+}
+</style>
